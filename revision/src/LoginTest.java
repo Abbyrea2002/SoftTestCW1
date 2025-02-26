1baf90314113af17714ee92fbe04e5437e6d840b
B00835054 <abbyrea2002@gmail.com>
2025-02-26 15:39:54 +0000

    set up

---
+++ revision/src/LoginTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class LoginTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\B00835054\\chromedriver-win64\\chromedriver.exe");
+                WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.co.uk/");
+        driver.manage().window().maximize();
+    }
+}
